1ee6f13224bababeb5ed779f856c1c0c8f246db6
jeongwonho64 <attemptn63@gmail.com>
2024-07-16 23:02:02 +0800

    big edits
    changed the algorithm a bit, edited animation

--- convexhull.js
+++ convexhull.js
@@ -2,32 +2,27 @@ function convexhull(){
     var p_0 = createVector(0,0);
     var hull = [];
     points.sort(function(a,b){return b.y-a.y});
-    p_0 = points[0];
-    textSize(32);
-    textAlign(CENTER,CENTER);
-    text("0",p_0.x,p_0.y);
+    p_0 = points[0].copy();
     points.shift();
     points.sort(function(a,b){
         angle_a = atan2(-(a.y-p_0.y),a.x-p_0.x);
         angle_b = atan2(-(b.y-p_0.y),b.x-p_0.x);
         return angle_a-angle_b;
     });
-    for(var i = 0;i<points.length;i++){
-        text(i+1,points[i].x,points[i].y);
-    }
-    hull.push(points[points.length-1]);
-    hull.push(p_0);
-    hull.push(points[0]);
+    points.unshift(p_0);
+    hull.push(points.length-1);
+    hull.push(0);
+    hull.push(1);
     var i = 1;
     while(i<points.length){
-        if(counter_clockwise(hull[hull.length-2],hull[hull.length-1],points[i])>0){
-            hull.push(points[i]);
+        if(counter_clockwise(points[hull[hull.length-2]],points[hull[hull.length-1]],points[i])>0){
+            hull.push(i);
             i++;
         }else{
             hull.pop();
         }
     }
-    hull.push(p_0);
+    hull.push(0);
     hull.shift();
     return hull;
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var editbutton,mode,cancelbutton,resetbutton,convexhullbutton;
 var points = [],hull = [];
 var startframe = 0, endframe = 0;
-var hull_loop = 0, percentage = 0;
-var line_start, line_end;
+var i_loop = 0, percentage = 0;
+var line_start, line_end, undraw;
 function setup(){
     createCanvas(windowWidth, windowHeight);
     background(255);
@@ -24,19 +24,23 @@ function createButtons(){
         mode = "edit";
     }
     cancelbutton.onPress = function(){
-        mode = "view";
+        if(mode == "edit"){
+            mode = "view";
+        }
     }
     resetbutton.onPress = function(){
         background(255);
         points = [];
         hull = [];
-        hull_loop = 0;
+        i_loop = 0;
         percentage = 0;
+        undraw = 0;
     }
     convexhullbutton.onPress = function(){
-        if(mode != "hull" && points.length > 2){
+        if(mode != "hull" && mode != "index" && points.length > 2){
             hull = convexhull(points);
-            mode = "hull";
+            mode = "index";
+            i_loop = 1;
         }
     }
     editbutton.text = "Edit";
@@ -50,14 +54,26 @@ function draw(){
     resetbutton.draw();
     convexhullbutton.draw();
     if(mode == "hull" && hull_loop < hull.length - 1){
-        if(percentage >= 1.029){
+        if(percentage >= 1.05){
             hull_loop++;
+            if(hull_loop == hull.length - 1){
+                percentage = 0;
+                return;
+            }
             percentage = 0;
         }
         line_start = hull[hull_loop].copy();
         line_end = hull[hull_loop + 1].copy();
         drawHull(hull_loop, percentage);
-        percentage += 0.03;
+        percentage += 0.05;
+    }
+    for(var i = 0; i < points.length; i++){
+        circle(points[i].x,points[i].y,60);
+        textSize(32);
+        textAlign(CENTER,CENTER);
+        if(mode == "hull"){
+            text(i,points[i].x,points[i].y);
+        }
     }
 }
 
@@ -67,16 +83,15 @@ function mousePressed(){
         points.push(createVector(mouseX,mouseY));
     }
 }
-function drawHull(i, percentage){
+function drawHull(hull_loop, percentage){
     var dist = line_start.dist(line_end);
-    var start = hull[i].copy();
-    start.lerp(line_end, 31/dist);
-    var end = hull[i+1].copy();
-    end.lerp(line_start, 31/dist);
-    start.lerp(end, percentage);
+    var start = line_start.copy();
+    var anim_end = start.copy();
+    var end = line_end.copy();
+    anim_end.lerp(end, percentage);
     push();
     strokeCap(SQUARE);
     strokeWeight(5);
-    line(start.x,start.y,end.x,end.y);
+    line(start.x,start.y,anim_end.x,anim_end.y);
     pop();
 }
